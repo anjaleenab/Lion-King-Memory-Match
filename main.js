a455f4ac47605a09a2ce40ae287dacbecb4750ed
Anjaleena Barclay <anjaleena2@aol.com>
2019-08-22 15:01:05 -0700

    finished feat 3 and started feat 4

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked= null;
 var matches = null;
-
-
+var max_matches =2;
 
 
 function initializeApp (){
@@ -12,31 +11,39 @@ $(".lfz-card").on("click", handleCardClick);
 
 }
 
+
+
+// var nextDivBackgroundUrls = [];
+// var lastDivBackgroundUrl = nextDivBackgroundUrls[nextDivBackgroundUrls.length - 1];
+// console.log(lastDivBackgroundUrl);
+// console.log(nextDivBackgroundUrls);
 function handleCardClick (event) {
 $(event.currentTarget).addClass("hidden");
+matchCards();
+}
+
+function matchCards() {
   console.log('Event:', event.currentTarget);
-  if (firstCardClicked == null) {
+  if (firstCardClicked === null) {
     firstCardClicked = $(event.currentTarget);
-    } if (firstCardClicked) {
-    secondCardClicked = $(event.currentTarget);
-    }
-var nextDivBackgroundUrl1 = firstCardClicked.next().css('background-image', 'url');
-console.log(nextDivBackgroundUrl1);
-var nextDivBackgroundUrl2 = secondCardClicked.next().css('background-image', 'url');
-
-
-  if(nextDivBackgroundUrl1 === nextDivBackgroundUrl2){
-    console.log('cards match');
-    matches++;
   } else {
-    setTimeout(function(){
-    firstCardClicked.removeClass('hidden');
-    secondCardClicked.removeClass('hidden');
-    console.log('class removed');
-    }, 1500);
+    secondCardClicked = $(event.currentTarget);
+    var nextDivBackgroundUrl1 = firstCardClicked.next().css('background-image');
+    var nextDivBackgroundUrl2 = secondCardClicked.next().css('background-image');
+      if (nextDivBackgroundUrl1 == nextDivBackgroundUrl2) {
+      console.log('cards match');
+      matches++;
+        if(matches ===max_matches) {
+
+        }
+    } else (nextDivBackgroundUrl1 !== nextDivBackgroundUrl2)
+      setTimeout(function () {
+        hideClasses();
+        console.log('class removed');
+      }, 1500);
+    }
   }
-
-
-
-
-  }
+    function hideClasses(){
+      firstCardClicked.removeClass('hidden');
+      secondCardClicked.removeClass('hidden');
+    }
