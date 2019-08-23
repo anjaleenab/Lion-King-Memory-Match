cca3d2e181ccabf1c7b7c6795b42760f166e4198
Anjaleena Barclay <anjaleena2@aol.com>
2019-08-22 18:25:11 -0700

    committing to master one last time

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(initializeApp);
 
+debugger;
 var firstCardClicked = null;
 var secondCardClicked= null;
 var matches = null;
 var max_matches =2;
-
+var clickCounter = 0;
 
 function initializeApp (){
 $(".lfz-card").on("click", handleCardClick);
@@ -23,26 +24,34 @@ matchCards();
 }
 
 function matchCards() {
+
   console.log('Event:', event.currentTarget);
-  if (firstCardClicked === null) {
+  if (clickCounter === 0) {
     firstCardClicked = $(event.currentTarget);
-  } else {
+    clickCounter++;
+  } else if(clickCounter > 0){
     secondCardClicked = $(event.currentTarget);
     var nextDivBackgroundUrl1 = firstCardClicked.next().css('background-image');
     var nextDivBackgroundUrl2 = secondCardClicked.next().css('background-image');
+    console.log(nextDivBackgroundUrl1);
+    console.log(nextDivBackgroundUrl2);
       if (nextDivBackgroundUrl1 == nextDivBackgroundUrl2) {
       console.log('cards match');
       matches++;
-        if(matches ===max_matches) {
 
-        }
-    } else (nextDivBackgroundUrl1 !== nextDivBackgroundUrl2)
+        if(matches === max_matches){
+      var modal = $('modal').addClass('modal').css('display:', 'show').text('Congratulations, you won!');
+      }
+    } else if (nextDivBackgroundUrl1 !== nextDivBackgroundUrl2){
       setTimeout(function () {
         hideClasses();
         console.log('class removed');
       }, 1500);
+    clickCounter = 0;
+      }
     }
   }
+
     function hideClasses(){
       firstCardClicked.removeClass('hidden');
       secondCardClicked.removeClass('hidden');
